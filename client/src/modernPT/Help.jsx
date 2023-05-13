a99d1bc1fb28c009b507ed80f67c21064a429686
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-05-12 17:35:35 -0700

    updated provider info and help sections

--- client/src/modernPT/Help.jsx
+++ client/src/modernPT/Help.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Help = () => {
   return (
     <div className= 'help-container'>
-      Help
+      <div className= 'help-title-container'>
+        <h2>Need Help?</h2>
+      </div>
+
+      <div className= 'help-button-container'>
+        <button className= 'help-button'>Chat</button>
+        <button className= 'help-button'>Email</button>
+        <button className= 'help-button'>Call</button>
+      </div>
+
     </div>
   )
 }
--- client/src/modernPT/ProviderInfo.jsx
+++ client/src/modernPT/ProviderInfo.jsx
@@ -3,9 +3,23 @@ import React from 'react'
 const ProviderInfo = () => {
   return (
     <div className= 'provider-info-container'>
-      Provider Info
+      <div className= 'provider-grid-container'>
+        <div className= 'provider-picture-container'>
+          Picture
+        </div>
+        <div className= 'provider-status-container'>
+          <h3> status</h3>
+          <h3> today's availability</h3>
+          <h2>Schedule Appointment</h2>
+        </div>
+      </div>
+
     </div>
   )
 }
 
 export default ProviderInfo
+
+{/* <div className= 'provider-button-container'>
+
+</div> */}
