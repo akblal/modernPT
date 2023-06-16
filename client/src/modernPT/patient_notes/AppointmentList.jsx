3de81c430f73597818bd111fd5b06dc0b00131c4
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-06-16 16:07:00 -0700

    style changes

--- client/src/modernPT/patient_notes/AppointmentList.jsx
+++ client/src/modernPT/patient_notes/AppointmentList.jsx
@@ -35,34 +35,3 @@ const AppointmentList = ({ note, setNote, currentNotes, totalNotes, currentPage,
 }
 
 export default AppointmentList
-
-  //  <div
-  //               key= {note.id}
-  //               className= {selectedNote === note.id ? 'selected-appointment-log-summary-container': 'appointment-log-summary-container'}
-  //               onClick= {() => {
-  //                 setSelectedNote(note.id)
-  //                 setNote(note)
-  //                 setEditChat({});
-  //                 setOption('')
-  //                 setMessage('')
-  //               }}
-  //               onMouseOver= {() => setHoveredNote(note.id)}
-  //               onMouseOut= {() => setHoveredNote(-1)}>
-  //               <div className= 'appointment-log-left-container'>
-  //                 <img className= 'appointment-log-therapist-picture-container' src={require('../../images/therapist_profile_pic/brandon_hsu.png')} alt= 'therapist_profile_pic' />
-  //               </div>
-  //               <div className=  'appointment-log-right-container'>
-  //                 <div className= 'appointment-log-top-right-container'>
-  //                   <div className= 'appointment-log-appointment-type'> #{totalNotes - (currentPage - 1) * notesPerPage - index } {note.appointment_type} </div>
-  //                   <div className= 'appointment-log-time-stamp'>{new Date(note.date_written).toLocaleString().substring(0, new Date(note.date_written).toLocaleString().indexOf(','))}</div>
-  //                 </div>
-  //                 <div className= 'appointment-log-bottom-container'>
-  //                   <div className= 'appointment-log-reason-container'>
-  //                     <div>Reason: Right Knee Pain Knee knee knee </div>
-  //                   </div>
-  //                   <div className= 'appointment-log-arrow-container'>
-  //                     <ImArrowRight2 className= {hoveredNote === note.id && selectedNote != hoveredNote? 'appointment-log-right-arrow bounce' : 'appointment-log-right-arrow hidden-arrow'}/>
-  //                   </div>
-  //                 </div>
-  //               </div>
-  //             </div>
